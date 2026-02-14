b59d8b96a23c4f78a1ba6c65dcfd3612695d2477
edperezh <edperezh@unal.edu.co>
2026-02-14 11:49:14 -0500

    commit para comentario del BMI

--- calcular_BMI.py
+++ calcular_BMI.py
@@ -8,3 +8,5 @@ peso_lb = round ((float(input('introduce el peso en libras de la persona: '))),2
 altura_inch = round ((float(input('introduce la altura en pulgadas de la persona: '))),2)
 
 print (calcular_BMI(peso_lb, altura_inch))
+
+# Como el peso es en libra la tabla para verificar tu BMI es otra que con el peso en kilogramos
